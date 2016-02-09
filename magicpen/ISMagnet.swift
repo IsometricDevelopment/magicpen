2f39b2e5611d03a183c74770fed13b654e316bc5
Joshua Liu <joshliu6830@gmail.com>
2016-02-08 21:02:37 -0500

    setting up files

---
+++ magicpen/ISMagnet.swift
@@ -0,0 +1,13 @@
+//
+//  ISMagnet.swift
+//  magicpen
+//
+//  Created by Joshua Liu on 2/5/16.
+//  Copyright © 2016 Isometric Studios. All rights reserved.
+//
+
+import UIKit
+
+class ISMagnet: NSObject {
+
+}
---
+++ magicpen/ISMagnetView.swift
@@ -0,0 +1,21 @@
+//
+//  ISMagnetView.swift
+//  magicpen
+//
+//  Created by Joshua Liu on 2/5/16.
+//  Copyright © 2016 Isometric Studios. All rights reserved.
+//
+
+import UIKit
+
+class ISMagnetView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
